8ed6e18bc120590144e414ccb2315171faf854be
P.Rakesh <padalabantu@gmail.com>
2025-10-28 11:47:51 +0530

    initial commit

---
+++ wordslist.py
@@ -0,0 +1,13 @@
+words = [
+    "lion", "tiger", "elephant", "giraffe", "zebra", "monkey",
+    "kangaroo", "panda", "leopard", "cheetah", "hippopotamus",
+    "rhinoceros", "alligator", "crocodile", "chimpanzee",
+    "gorilla", "koala", "sloth", "penguin", "ostrich", "flamingo",
+    "peacock", "eagle", "parrot", "owl", "sparrow", "dolphin",
+    "whale", "shark", "octopus", "seal", "walrus", "turtle",
+    "snake", "lizard", "frog", "toad", "butterfly", "bee",
+    "ant", "spider", "scorpion", "camel", "donkey", "horse",
+    "sheep", "goat", "pig", "cow", "dog", "cat", "rabbit",
+    "deer", "fox", "wolf", "bear", "bat", "hedgehog", "squirrel",
+    "otter", "raccoon", "moose", "buffalo", "hyena", "jaguar"
+]
